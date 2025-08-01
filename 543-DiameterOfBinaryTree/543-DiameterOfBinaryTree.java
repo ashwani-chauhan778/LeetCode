f1010b982573d184b550f65fca5f10bba7942955
ashwani-chauhan778 <cashwani321@gmail.com>
2025-08-01 14:41:23 +0530

    Create 543-DiameterOfBinaryTree

---
+++ 543-DiameterOfBinaryTree/543-DiameterOfBinaryTree.java
@@ -0,0 +1,19 @@
+// Last updated: 8/1/2025, 2:41:23 PM
+class Solution {
+  public int diameterOfBinaryTree(TreeNode root) {
+    maxDepth(root);
+    return ans;
+  }
+
+  private int ans = 0;
+
+  int maxDepth(TreeNode root) {
+    if (root == null)
+      return 0;
+
+    final int l = maxDepth(root.left);
+    final int r = maxDepth(root.right);
+    ans = Math.max(ans, l + r);
+    return 1 + Math.max(l, r);
+  }
+}
